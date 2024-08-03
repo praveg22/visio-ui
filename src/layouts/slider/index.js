7d66ba4bdcbe58a19d70b95c4d241967d2e6f4f9
praveg22 <abhi443612@gmail.com>
2024-08-03 14:45:57 +0530

    update images

--- src/layouts/slider/index.js
+++ src/layouts/slider/index.js
@@ -1,17 +1,17 @@
 import { Carousel } from "react-bootstrap";
 import img1 from "./img1.png";
-import img2 from "./img2.jpg";
-import img3 from "./img3.jpg";
-import img4 from "./img4.jpg";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
+import img4 from "./img4.png";
 import img5 from "./img5.jpg";
 import img6 from "./img6.jpg";
-import img7 from "./img7.jpg";
-import img8 from "./img8.jpeg";
+// import img7 from "./img7.jpg";
+// import img8 from "./img8.jpeg";
 import "./Slider.css";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8];
+const images = [img1, img2, img3, img4, img5, img6];
 
 const Slider = () => (
   <>
